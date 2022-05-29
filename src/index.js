ede44088475989b62e150f221b06de93a0bebf9b
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-29 13:54:50 -0600

    Pinged API
    Quick code to determine that the api has the right key and is communicating with our site.
    
    Co-Authored-By: ElliotMcGonigal <84742205+ElliotMcGonigal@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function(){
+$(document).ready(function() {
+  $('#load').click(function() {
+    console.log("button clicked");
+    let request = new XMLHttpRequest();
+    const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&callback=initMap&v=weekly`;
 
-  
+    request.onreadystatechange = function() {
+      console.log(this.readyState);
+    };
+
+    request.open("GET", url, true);
+    request.send();
+  });
 });
